b8833a83ba6a01a6069e000ed32794f7f0765c24
Nruggz <86481527+Nruggz@users.noreply.github.com>
2024-02-13 07:28:20 -0500

    Update

---
+++ src/db.js
@@ -0,0 +1,28 @@
+// Require the mongoose library
+const mongoose = require('mongoose');
+
+module.exports = {
+    connect: DB_HOST => {
+        // Use the Mongo driver's updated URL string parser
+        mongoose.set('useNewUrlParser', true);
+        // Use findOneAndUpdate() in a place of findAndModify()
+        mongoose.set('useFindAndModify', false);
+        // Use createIndex() in a place of ensureIndex()
+        mongoose.set('useCreateIndex', true);
+        // Use the new server discovery and monitoring engine
+        mongoose.set('useUnifiedTopology', true);
+        // Connect to the DB
+        mongoose.connect(DB_HOST);
+        // Log an error if we fail to connect
+        mongoose.connection.on('error', err => {
+            console.log(err);
+            console.log(
+                'MongoDB connection error. Please make sure MongoDB is running.'
+            );
+            process.exit();
+        });
+    },
+    close: () => {
+        mongoose.connection.close();
+    }
+};
